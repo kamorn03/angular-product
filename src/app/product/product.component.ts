afcf177219515d916e909021de25eef41e241067
Kamornpisit <ilimiocreate@gmail.com>
2024-01-16 22:12:54 +0700

    fix: search method:

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -20,7 +20,7 @@ export class ProductComponent implements OnInit, OnDestroy {
   isActive: string | undefined;
 
   // value of search
-  name: string | undefined;
+  name: string | null | undefined;
   selectedGroup: string | null | undefined;
   selectedSubGroup: string | null | undefined;
   selectedActive: string | null | undefined;
@@ -54,11 +54,9 @@ export class ProductComponent implements OnInit, OnDestroy {
         }
       );
     });
-
   }
 
   onSelectGroupChanged() {
-    console.log('LOG group : ', this.selectedGroup)
     if (this.selectedGroup) {
       this.productService.getSubGroupฺฺByGroup(Number(this.selectedGroup)).subscribe(
         (data: SubGroup[]) => {
@@ -72,18 +70,18 @@ export class ProductComponent implements OnInit, OnDestroy {
   }
 
   onSelectSubGroupChanged() {
-    console.log('LOG sub : ', this.selectedSubGroup)
+    // console.log('LOG sub : ', this.selectedSubGroup)
   }
+
   // search !!!!!!! ****
   searchData() {
     // value of search
     const data = {
       name: this.name,
-      groupId: Number(this.selectedGroup),
-      subGroupId: Number(this.selectedSubGroup),
+      groupId: Number(this.selectedGroup) === 0 || this.selectedGroup === null ? null : Number(this.selectedGroup),
+      subGroupId: Number(this.selectedSubGroup) === 0 || this.selectedSubGroup === null ? null : Number(this.selectedSubGroup),
       isActive: this.selectedActive ? this.selectedActive === 'active' ? true : false : null
     }
-
     this.productService.searchDataApi(data).subscribe(
       (data: Product[]) => {
         this.product = data;
@@ -97,11 +95,20 @@ export class ProductComponent implements OnInit, OnDestroy {
   // clear !!!!!!! ****
   clearData() {
     // value of search
-    this.selectedSubGroup = undefined; 
+    this.selectedSubGroup = null;
+    this.selectedGroup = null;
+    this.name = null;
+    this.selectedActive = null;
+
+    this.productService.getProducts().subscribe(
+      (data: Product[]) => {
+        this.product = data;
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
   }
-
-
-
 
   onSelectedItemChanged(itemId: number) {
     this.selectedItemId = itemId;
